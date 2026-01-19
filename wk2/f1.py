fc643925504345a0192d0e535a4343f04e23d13e
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2026-01-19 09:41:57 -0500

    Create f1.py

---
+++ wk2/f1.py
@@ -0,0 +1,11 @@
+# print("Hello")
+# print('World')
+# = comment = code won't be executed
+# print("Hello", 'World')
+# print("Hello", 'World', "!", sep='***')
+print("Hello " * 3) # repeat a string value 
+print()  # output a new line
+print("I am", 100, "years old!", "The temp outside is",
+      -10.5, "degrees")
+
+
