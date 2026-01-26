b889eb1527a4b86a5e5a93bc27e2654c04f2e4af
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2026-01-26 10:51:42 -0500

    Create bool_exp.py

---
+++ wk3/bool_exp.py
@@ -0,0 +1,32 @@
+"""
+
+8
+    1000
+4
+    0100
+
+bit operators:
+& => if both bits are 1 => result is one
+| if either 1, res 1
+^ xor only 1 bit is 1 for redult to be 1
+"""
+print(8 ^ 4)  # bit-wise operations
+
+print("*" * 20)
+print('b' in "ben")  # membership operations
+print('a' not in "ben")  # membership operations
+
+print( 1 < 10)
+print( 1 <= 1)
+print(10 >= 3 * 3 + 1 and 'e' in 'example')
+print(100 < 10 or 'c' in 'comp1250' and 2 ** 3 > 3 * 2)
+# 100 < 10 or True and 2 ** 3 > 3 * 2
+# 100 < 10 or True and True
+# 100 < 10 or True
+# False or True
+
+
+"""
+in, and, or, not
+reg bedmas
+"""
